688efd8ef7e47f0e4baaf4a8e0206f06a3f2f467
Bhavya Marya <bhavyamarya2002@gmail.com>
2024-09-04 14:26:44 +0530

    bean

---
+++ JEDI-Flipkart-Group-A-Flipfit-POS/src/com/flipkart/bean/FlipFitNotifications.java
@@ -0,0 +1,32 @@
+package com.flipkart.bean;
+
+public class FlipFitNotifications {
+    private String category;
+    private String message;
+    private int notificationId;
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public int getNotificationId() {
+        return notificationId;
+    }
+
+    public void setNotificationId(int notificationId) {
+        this.notificationId = notificationId;
+    }
+
+}
